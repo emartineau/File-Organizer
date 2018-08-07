4c13554152de862dfc8ba02882d52f64a4ae1717
Eric <emartineau52@gmail.com>
2018-08-07 03:22:16 -0400

    Add AppCommands - contains commands for model
    Unlike Commands this class makes direct changes to the model

---
+++ FileOrganizer/Model/AppCommands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOrganizer.Model
+{
+    static class AppCommands
+    {
+        public delegate void AppDel(ref Organizer organizer);
+
+        private static void ToParentDirectory(ref Organizer organizer)
+        {
+            organizer.WorkingDirectory
+        }
+    }
+}
